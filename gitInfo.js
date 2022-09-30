cb947776adb6e5e07b8a97e2afc7b93ae63abfc4
Austin Warnecke <warneckeaustin@gmail.com>
2022-09-30 11:36:57 -0600

    assessment-gitinfo

--- gitInfo.js
+++ gitInfo.js
@@ -11,6 +11,9 @@
 
 //CODE HERE
 
+gitDefinition = "Git is an app (not a website) that lets you control and manage your code using the terminal"
+//    console.log(gitDefinition)
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,6 +21,9 @@
 */
 
 //CODE HERE
+
+gitHubDefinition = "gitHub is a website that lets you manage repositories created in Git (the app). These respositories can be set to public or private."
+//    console.log(gitHubDefinition)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -27,6 +33,9 @@
 
 //CODE HERE
 
+gitInitDefinition = "Git Init allows the user to initialize a Git repository to be sent to GitHub."
+//    console.log(gitInitDefinition)
+
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -35,6 +44,9 @@
 
 //CODE HERE
 
+gitCloneDefinition = "Git Clone will allow the user to copy a current repository and add it to a new folder in a different location"
+//    console.log(gitCloneDefinition)
+
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,6 +54,9 @@
 */
 
 //CODE HERE
+
+gitStatusDefinition = "Git Status will allow the user to see which status the repository they are working is currently in."
+//    console.log(gitStatusDefinition)
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -54,6 +69,12 @@
 
 //CODE HERE
 
+gitAddDefinition = "Git Add (git add .) allows the files to be tracked. This is a required step to allow the code to be pushed to the repository"
+//    console.log(gitAddDefinition)
+
+gitAddCode = "git add ."
+//  console.log(gitAddCode)
+
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -65,6 +86,11 @@
 
 //CODE HERE
 
+gitCommitDefinition = "Git Commit allows the user to create a current snapshot of the code to send to the resposity in GitHub."
+//    console.log(gitCommitDefinition)
+
+gitCommitCode = "git commit -m 'initial commit'"
+//    console.log(gitCommitCode)
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +99,6 @@
 */
 
 //CODE HERE
+
+gitPushDefinition = "Git Push allows the snapshot taken with Git Commit to be pushed to the respository in GitHub to be viewed as public or private."
+//    console.log(gitPushDefinition)

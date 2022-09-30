a0309607dc84b695ad7c9a11d70e7bc82d74b4d3
Austin Warnecke <warneckeaustin@gmail.com>
2022-09-30 11:13:31 -0600

    assessment-arrays

--- arrays.js
+++ arrays.js
@@ -5,10 +5,17 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+const colorCopy = faveColors.slice(0, 3)
+//  console.log (colorCopy)
+
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+colorCopy.push("blue")
+//  console.log(colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -20,6 +27,8 @@ const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
 
+middleNums =  numbers.slice(1,4)
+//  console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +43,12 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+
+answers = []
+
+for (let i=0; i<bigOrSmallArray.length; i++) {
+  if(bigOrSmallArray.length > 100)
+  console.log("big")
+} else {
+  console.log("small")
+}
